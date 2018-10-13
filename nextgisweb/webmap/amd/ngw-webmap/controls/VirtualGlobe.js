8b68407314de2473722380ca7da9a7fdf9d242ba
Artemiy <rendrom@gmail.com>
2018-10-14 02:50:19 +0800

    ol-cesium build to umd

--- nextgisweb/webmap/amd/ngw-webmap/controls/VirtualGlobe.js
+++ nextgisweb/webmap/amd/ngw-webmap/controls/VirtualGlobe.js
@@ -1,15 +1,18 @@
-define([
-    'dojo/_base/declare',
-    "dojo/on",
-    "dojo/dom-construct",
-    "dojo/string",
-    'ngw-pyramid/i18n!webmap',
-    'ngw-pyramid/hbs-i18n',
-    "openlayers/ol"
-], function (declare, on, domConstruct, string, i18n, hbsI18n, ol) {
+define(function (require) {
+
+    var declare = require('dojo/_base/declare');
+    var on = require("dojo/on");
+    var domConstruct = require("dojo/dom-construct");
+    var string = require("dojo/string");
+    var i18n = require('ngw-pyramid/i18n!webmap');
+    var hbsI18n = require('ngw-pyramid/hbs-i18n');
+    var ol = require("openlayers/ol");
+
     return declare(ol.control.Control, {
         constructor: function(options) {
             var widget = this;
+
+            this.enabled = false;
 
             this.inherited(arguments);
             declare.safeMixin(this,options);
@@ -27,13 +30,26 @@ define([
             });
 
             on(this.element, "click", function(){
-                console.log(widget.display.map.olMap);
+                if (!window.Cesium) {
+                    require(['openlayers/Cesium/Cesium'], function (dep) {
+                        widget.ol3d = new olcs.OLCesium({ map: widget.display.map.olMap }); // map is the ol.Map instance
+                        widget.toggleCesium();
+                    });
+                } else {
+                    widget.toggleCesium();
+                }
+                // console.log(widget.display.map.olMap);
             });
 
             ol.control.Control.call(this, {
                  element: this.element,
                  target: this.target
             });
+        },
+
+        toggleCesium: function () {
+            this.enabled = !this.enabled;
+            this.ol3d.setEnabled(this.enabled);
         }
     });
 });
